d8c52bc395697b0c755196dc7e83aa92b11bcbc7
Aslihan Genc <aslhngencc@gmail.com>
2023-04-30 19:23:07 +0300

    finish

---
+++ src/components/Info.jsx
@@ -0,0 +1,10 @@
+function Info() {
+    return (
+        <footer className="info">
+            <p>ToDo App</p>
+            <p>Designed by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
+        </footer>
+    )
+}
+
+export default Info
